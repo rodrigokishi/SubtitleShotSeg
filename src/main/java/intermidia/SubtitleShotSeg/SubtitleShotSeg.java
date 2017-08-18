12fd277f8f511e016c9311f2666bd4b55dda8a4c
rodrigokishi <rodrigokishi@usp.br>
2017-08-18 19:06:28 -0300

    Initial commit

---
+++ src/main/java/intermidia/SubtitleShotSeg/SubtitleShotSeg.java
@@ -0,0 +1,10 @@
+package intermidia.SubtitleShotSeg;
+
+
+public class SubtitleShotSeg 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
