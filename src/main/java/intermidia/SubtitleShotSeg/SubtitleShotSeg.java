01e11a30fa65c4874207e6a07fd71ac51ddc1e45
Rodrigo Kishi <rodrigokishi@usp.br>
2017-08-21 11:59:02 -0300

    First version.

--- src/main/java/intermidia/SubtitleShotSeg/SubtitleShotSeg.java
+++ src/main/java/intermidia/SubtitleShotSeg/SubtitleShotSeg.java
@@ -1,10 +1,32 @@
 package intermidia.SubtitleShotSeg;
 
+import java.io.File;
+
+import com.googlecode.mp4parser.srt.SrtParser;
 
 public class SubtitleShotSeg 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SrtParser srtParser = new SrtParser();
+    	//srtParser.parse(new );
     }
 }
+
+
+/*import org.fredy.jsrt.api.SRT;
+import org.fredy.jsrt.api.SRTInfo;
+import org.fredy.jsrt.api.SRTReader;
+
+public class SubtitleShotSeg 
+{
+    public static void main( String[] args )
+    {
+        SRTInfo info = SRTReader.read(new File(args[0]));
+        for(SRT sub : info )
+        {
+        	System.out.println(sub.text);
+        }
+    }
+}
+*/
